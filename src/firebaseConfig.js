2338c438c8468983ae7a782e9d807d9e68741d35
nwessman <nwessman@kth.se>
2022-04-04 22:44:35 +0200

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyCS1G5aAtpFzXHNn-RKU9qwEb59sB0g6CQ",
+  authDomain: "travelbuddy-700b8.firebaseapp.com",
+  databaseURL: "https://travelbuddy-700b8-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "travelbuddy-700b8",
+  storageBucket: "travelbuddy-700b8.appspot.com",
+  messagingSenderId: "981322356565",
+  appId: "1:981322356565:web:4350ef6f5aebf136a881dc"
+};
