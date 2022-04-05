621c179bd7a43ce8449a13a259770dbb077d7365
Daniel H <dhartler@kth.se>
2022-04-05 14:49:57 +0200

    script adding city info to firebase

---
+++ dataToFirebase/addCityGeoLocationFirebase.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import json
+
+data= pd.read_csv("worldcities.csv", usecols=["city", "lat", "lng"])
+cities = json.loads(data.to_json(orient='records'))
+
+print(cities)
+
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('../webscraping/travelbuddy-700b8-firebase-adminsdk-rimpp-d82b8f7841.json')
+
+# Initialize the app with a custom auth variable, limiting the server's access
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://travelbuddy-700b8-default-rtdb.europe-west1.firebasedatabase.app',
+
+})
+
+# The app only has access as defined in the Security Rules
+ref = db.reference("Cities") #'travelbuddy-700b8-default-rtdb'
+
+ref.set(cities)
