9ebe9b6883ae9fd0a8f979e1ccb6c6984b6556a5
nwessman <nwessman@kth.se>
2022-04-05 23:31:03 +0200

    Fix: Correct JSON output that works in ffirebase

--- webscraping/combineCities.py
+++ webscraping/combineCities.py
@@ -9,11 +9,11 @@ f = open("cityoutput.txt", "r", encoding='utf-8')
 cities = json.loads(f.read())
 f.close()
 
-output = "{"
 
+dict = {}
 
 for cityInfo in cities:
-  city = cityInfo["city"]
+  city = cityInfo["city"].replace('.', '').replace('/','')
   long = cityInfo["lng"]
   lat = cityInfo["lat"]
   airport = []
@@ -21,32 +21,14 @@ for cityInfo in cities:
     for airportCity in airportInfo["Cities"]:
       if(city.strip() == airportCity.strip()):
         airport.append(airportInfo["AITA"])
-  output +=  '"'+city+'":{'
-  output += '"lat" : "' + str(lat) + '",'
-  output += '"lng" : "' + str(long) + '"'
+
+  dict[city] = {
+    "lng" : long,
+    "lat" : lat,
+  }
   if(len(airport) > 0):
-    output += ',"airport" : ['
-    for index, value in enumerate(airport):
-      output += '"'+value+'"'
-      if(index < len(airport) - 1):
-        output += ','
-    output += ']'
-  output += "},"
+    dict[city]["airport"] = airport
 
-  #print("city: " + city + " long: " + str(long) + " lat: " + str(lat) + " airport: " + str(airport))
-
-
-for key in airports:
-  #print(str(key["Cities"]))
-  for city in key["Cities"]:
-    #print(city)
-    if("Tokyo" == city.strip()):
-      print("Hit!: " + str(key))
-
-output = output[:-1]
-output += "}" 
-
-
-f = open("combinedOutput.txt","w", encoding='utf-8')
-f.write(output)
+f = open("combinedOutput.json","w", encoding='utf-8')
+f.write(json.dumps(dict, indent = 3))
 f.close() 
